1d1177f91ec63db606ff4fe0efbbdbdbb0c1168b
bda-mota <bea.mota04@gmail.com>
2024-04-25 13:51:39 -0300

    função read_line criada e sem leaks

--- sources/main.c
+++ sources/main.c
@@ -1,32 +1,39 @@
 #include "../includes/minishell.h"
 
+static void	read_line(t_token **token_t, t_token **token_h);
+
 int	main(void)
 {
 	t_token	*token_t;
 	t_token	*token_h;
 
+
 	init_token(&token_t, &token_h);
-	insert_token(&token_h, "bia");
-	insert_token(&token_h, "B1");
-	insert_token(&token_h, "B2");
+	read_line(&token_t, &token_h);
 	print_list(&token_t);
 	deallocate_lst(&token_t, &token_h);
 	exit(1);
 }
 
-	//char	*retorno;
+static void	read_line(t_token **token_t, t_token **token_h)
+{
+	char	*input;
 
-	/*while (1)
+	(void)token_t;
+	while (1)
 	{
-		retorno = readline("$minishell: ");
-		if (retorno == NULL)
+		input = readline("$minishell: ");
+		if (input == NULL)
 		{
 			printf("Erro ao ler a linha\n");
 			continue ;
 		}
-		if (ft_strcmp(retorno, "exit") == 0)
-			break ;
-		insert_token(&token, retorno);
-		printf("token: %s\n", retorno);
-		free(retorno);
-	}*/
+		if (ft_strcmp(input, "exit") == 0)
+		{
+			free(input);
+			return ;
+		}
+		insert_token(token_h, input);
+		free(input);
+	}
+}
