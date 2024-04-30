81f8042389f1370e3c4557988294090d6b16d5ef
Bianca Santana Souza <bsantana@c1r5p1.42sp.org.br>
2024-04-30 11:36:30 -0300

    alterações na catalog_inputs: testes para heredoc e append

--- sources/token/token.c
+++ sources/token/token.c
@@ -8,10 +8,14 @@ int	catalog_inputs(char *c, size_t *i)
 		return (PIPE);
 	else if (ft_strnchr(&c[*i], '"', 1) == 1)
 		return (DOUBLE);
-	// else if (c == '<')
-	// 	return (INPUT);
-	// else if (c == '>')
-	// 	return (OUTPUT);
+	else if (ft_strnchr(&c[*i], '<', 1) == 1)
+		return (INPUT);
+	else if (ft_strnchr(&c[*i], '>', 1) == 1)
+		return (OUTPUT);
+	else if (ft_strnchr(&c[*i], '<', 1) == 2)
+		return (HEREDOC);
+	// else if (ft_strnchr(&c[*i], '>', 1) == 2)
+	// 	return (APPEND);
 	else
 		return (WORD);
 }
@@ -35,6 +39,8 @@ void	course_inputs(t_token **token_h, char *input)
 			handle_io_input(token_h, input, &i);
 		else if (catalog_inputs(input, &i) == OUTPUT)
 			handle_io_input(token_h, input, &i);
+		// else if (catalog_inputs(input, &i) == HEREDOC)
+		// 	handle_heredoc(token_h, input, &i);
 		i++;
 	}
 }
