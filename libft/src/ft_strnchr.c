730788dcbfdf6961033eb5fae9f522c2eff1bb40
bda-mota <bea.mota04@gmail.com>
2024-04-30 14:57:45 -0300

    função strnchr alterada para lidar caso não tenha espaço

--- libft/src/ft_strnchr.c
+++ libft/src/ft_strnchr.c
@@ -6,7 +6,7 @@
 /*   By: bda-mota <bda-mota@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 13:50:32 by bda-mota          #+#    #+#             */
-/*   Updated: 2024/04/30 13:35:09 by bda-mota         ###   ########.fr       */
+/*   Updated: 2024/04/30 14:55:22 by bda-mota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,19 @@
 
 int	ft_strnchr(char *str, char c, int len)
 {
-	int	i;
-
-	i = 0;
-	while (*str && i < len)
+	if (len == 1)
 	{
-		if (*str == c && (i + 1 == len))
+		if (*str == c)
 			return (1);
-		str++;
-		i++;
+	}
+	else if (len == 2)
+	{
+		while (*str && *(str + 1))
+		{
+			if (*str == c && *(str + 1) == c)
+				return (1);
+			str++;
+		}
 	}
 	return (0);
 }
