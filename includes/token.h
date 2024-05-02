f8e5eade2fe5b8b2af8b4814c486b550ef1c09fa
Bianca Santana Souza <bsantana@c1r6p2.42sp.org.br>
2024-05-02 11:22:49 -0300

    comentários e ajuste na handle_pipe

--- includes/token.h
+++ includes/token.h
@@ -4,18 +4,69 @@
 # include "../libft/src/libft.h"
 # include "structs.h"
 
-//LINKED LISTS
+/**** linked list ****/
+
+/*
+ * Initializes a list of tokens.
+ *
+ * tail: pointer to the pointer to the last node in the list of tokens.
+ * head: pointer to the head pointer of the token list.
+ */
 void	init_token(t_token **tail, t_token **head);
+
+/*
+ * Releases the memory allocated to each node in the list and the list itself.
+ */
 void	deallocate_lst(t_token **tail, t_token **head);
+
+/*
+ * Adds a new node to the token list.
+ * 
+ * token_head: pointer to the head pointer of the token list.
+ * content: content to be inserted into the new node.
+ */
 void	insert_token(t_token **token_head, char *content);
+
+/*
+ * Removes the first node from the token list.
+ */
 void	remove_first(t_token **token);
 
-//TOKEN
-void	course_inputs(t_token **token_h, t_token **token_t, char *input);
+/**** inputs ****/
+
+/*
+ * Checks and classifies the type of input supplied.
+ * 
+ * Parameters:
+ * c: pointer to the string containing the input to be sorted.
+ * i: pointer to the current index within the input string.
+ *
+ * Return:
+ * Returns the input type.
+ */
 int		catalog_inputs(char *c, size_t *i);
 
-//SEPARATORS
+/*
+ * Identifies the type of entry to insert into the token list.
+ */
+void	course_inputs(t_token **token_h, t_token **token_t, char *input);
+
+/**** separators ****/
+
+/*
+ * Checks if there is a double quote (") in the current
+ * position in the 'input' string. If a double quote is found,
+ * the function counts the number of characters between the double
+ * quotes, including the quotes themselves.
+ * The total length of the string is returned.
+ */
 int		ft_handle_quote(char *input, size_t *i);
+
+/*
+ * Handle the processing of a quote in the
+ * input and insert it into the token list.
+ */
+
 void	handle_quote(t_token **token_h, char *input, size_t *i, size_t size);
 void	handle_word(t_token **token_h, char *input, size_t *i);
 void	handle_pipe(t_token **token_h, char *input, size_t *i);
