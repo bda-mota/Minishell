47110c2167dcb566f4523f33cef46dfb076660aa
bda-mota <bea.mota04@gmail.com>
2024-05-03 13:35:59 -0300

    replace input for outpput

--- sources/token/token.c
+++ sources/token/token.c
@@ -4,6 +4,10 @@ int	catalog_inputs(char *c, size_t *i)
 {
 	if (ft_isspace(c[*i]) == 1)
 		return (SSPACE);
+	else if (!ft_strnchr(&c[*i], '<', 1))
+		return (INPUT);
+	else if (!ft_strnchr(&c[*i], '>', 1))
+		return (OUTPUT);
 	else if (!ft_strnchr(&c[*i], '<', 3) || !ft_strnchr(&c[*i], '>', 3)
 		|| !ft_strnchr(&c[*i], '|', 3) || !ft_strnchr(&c[*i], '|', 2)
 		|| !ft_strnchr(&c[*i], '&', 2))
@@ -16,10 +20,6 @@ int	catalog_inputs(char *c, size_t *i)
 		return (APPEND);
 	else if (!ft_strnchr(&c[*i], '"', 1))
 		return (DOUBLE);
-	else if (!ft_strnchr(&c[*i], '<', 1))
-		return (INPUT);
-	else if (!ft_strnchr(&c[*i], '>', 1))
-		return (OUTPUT);
 	else if (!ft_strnchr(&c[*i], '(', 1) || !ft_strnchr(&c[*i], ')', 1))
 		return (BLOCK);
 	else
@@ -44,9 +44,9 @@ void	course_inputs(t_token **token_tail, t_token **token_h, char *input)
 		else if (catalog_inputs(input, &i) == PIPE)
 			handle_pipe(token_h, input, &i);
 		else if (catalog_inputs(input, &i) == INPUT)
-			handle_output(token_h, input, &i);
-		else if (catalog_inputs(input, &i) == OUTPUT)
 			handle_input(token_h, input, &i);
+		else if (catalog_inputs(input, &i) == OUTPUT)
+			handle_output(token_h, input, &i);
 		else if (catalog_inputs(input, &i) == HEREDOC)
 			handle_heredoc(token_h, input, &i);
 		else if (catalog_inputs(input, &i) == APPEND)
--- sources/trash.c
+++ sources/trash.c
@@ -6,7 +6,7 @@
 /*   By: bda-mota <bda-mota@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 14:17:58 by bsantana          #+#    #+#             */
-/*   Updated: 2024/05/02 18:33:15 by bda-mota         ###   ########.fr       */
+/*   Updated: 2024/05/03 13:35:40 by bda-mota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@ void	print_list(t_token **lst)
 	{
 		printf("- - - - - - - - - - - - - - \n");
 		printf("token: %s\n", current->content);
-		printf("type: %d\n", current->type);
+		printf("type: %s\n", current->type);
 		printf("- - - - - - - - - - - - - - \n");
 		current = current->next;
 	}
 }
+
+// static char *find_type(int type)
+// {
+
+// }
