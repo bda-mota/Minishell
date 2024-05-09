76f6e2693277afe0a2ef19e8f11e3ad64a95e3f7
Bianca Santana Souza <bsantana@c3r3p5.42sp.org.br>
2024-05-09 13:59:53 -0300

    alterações na bloom_tree

--- sources/ast/build_tree.c
+++ sources/ast/build_tree.c
@@ -1,5 +1,21 @@
 #include "../../includes/minishell.h"
 
+t_tree	*build_tree(t_token **tokens, t_tree **root)
+{
+	t_tree	*new;
+	int		delimiter;
+
+	if (find_delimiter(*tokens, PIPE) == 1)
+		delimiter = PIPE;
+	else
+		delimiter = INPUT;
+
+	// percorrendo e se for search -> PIPE bloom_tree
+
+	new = search_delimiter(create_root(*tokens), delimiter);
+	bloom_tree(*root, new, LEFT);
+}
+
 t_tree	*create_root(t_token *tokens)
 {
 	t_tree	*new_root;
@@ -12,6 +28,20 @@ t_tree	*create_root(t_token *tokens)
 	new_root->left = NULL;
 	new_root->right = NULL;
 	return (new_root);
+}
+
+int	find_delimiter(t_token **tokens, int delimiter)
+{
+	t_token	*curr;
+
+	curr = get_last_node(*tokens);
+	while (curr)
+	{
+		if (curr->type == delimiter)
+			return (1);
+		curr = curr->prev;
+	}
+	return (0);
 }
 
 t_tree	*search_delimiter(t_token **tokens, int delimiter)
@@ -27,17 +57,6 @@ t_tree	*search_delimiter(t_token **tokens, int delimiter)
 	}
 	return (NULL);
 }
-
-t_tree	*build_tree(t_token **tokens, t_tree **root)
-{
-	t_tree	*new;
-
-	// percorrendo e se for search -> PIPE bloom_tree
-
-	new = search_delimiter(create_root(*tokens), PIPE);
-	bloom_tree(*root, new, RIGHT);
-}
-
 // lembrar de n mandar a raíz da árvore , mas sim o nó onde queremos adicionar à direita ou à esquerda
 
 void	bloom_tree(t_tree **root, t_tree *new_branch, int side)
@@ -53,10 +72,11 @@ void	bloom_tree(t_tree **root, t_tree *new_branch, int side)
 			curr = curr->left;
 		curr->left = new_branch;
 	}
-	else if (side == RIGHT)
+	else
 	{
 		while (curr->right != NULL)
 			curr = curr->right;
 		curr->right = new_branch;
 	}
+	return ;
 }
