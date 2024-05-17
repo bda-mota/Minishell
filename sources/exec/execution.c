856d5ebac507826c30a218b347a90e7616c3c696
bda-mota <bea.mota04@gmail.com>
2024-05-17 11:34:40 -0300

    Co-authored-by: Bianca Santana <biancasantana81@hotmail.com>

--- sources/exec/execution.c
+++ sources/exec/execution.c
@@ -6,10 +6,10 @@ char	*check_command(t_exec *execution)
 	char	*command;
 	char	*complete_command;
 
-	command = pipex->argv_child[0];
+	command = execution->argv_child[0];
 	if (access(command, X_OK) == 0)
 		return (command);
-	complete_command = add_cmd_env(pipex, command);
+	complete_command = add_cmd_env(execution, command);
 	return (complete_command);
 }
 =======
