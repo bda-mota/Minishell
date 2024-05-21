c39c9e83daa4cc59b0b0017c8d15a32df6b4dccc
Bianca Santana Souza <bsantana@c1r4p3.42sp.org.br>
2024-05-21 12:08:02 -0300

    fix norminette, add __environ macro

--- includes/exec.h
+++ includes/exec.h
@@ -11,11 +11,10 @@ char	*add_command_to_path(t_exec **execution, char *cmd);
 
 /* EXPANSION */
 char	*get_expand_variable(t_token **token);
-char    **count_size_environ();
-char    **copy_environ();
-char    **create_new_environ(char **my_environ, int num_vars);
-char    **add_variable_to_environ(char **my_environ, char *new_var);
-void    free_environ(char **environ);
+char	**count_size_environ(void);
+char	**copy_environ(void);
+char	**create_new_environ(char **my_environ, int num_vars);
+char	**add_variable_to_environ(char **my_environ, char *new_var);
 
 /* EXECUTION */
 void	implement(t_exec **execution, char *command);
--- sources/exec/export.c
+++ sources/exec/export.c
@@ -1,44 +1,41 @@
 #include "../../includes/minishell.h"
 
 /*
-1. environ não sera utilizada, apenas foi implementada aqui pois no meu Ubuntu 
-ela não aparece como uma macro de ambiente na 42 ela aparece e deve ser modificada.
-2. Antes de encerrar o programa ainda há a necessidade de liberar a matriz com as variáveis de ambiente
+1. Antes de encerrar o programa ainda há a necessidade de liberar a matriz com as variáveis de ambiente
 do nosso BabyShell -> usar estrutura do main;
-3. Esta é apenas a estrutura da criação de novas variáveis -> ela precisa ser ajustada assim como
+2. Esta é apenas a estrutura da criação de novas variáveis -> ela precisa ser ajustada assim como
 as outras builtins.
-4. Ao final do arquivo há um main teste para verificar o funcionamento da export separadamente do BabyShell.
+3. Ao final do arquivo há um main teste para verificar o funcionamento da export separadamente do BabyShell.
 */
 
-char    **export(char *new_variable)
+char	**export(char *new_variable)
 {
-    char **original_environ;
-    char **new_environ;
+	char	**original_environ;
+	char	**new_environ;
 
-    original_environ = copy_environ();
-    new_environ = add_variable_to_environ(original_environ, new_variable);
-    if (!new_environ)
-    {
-        free_environ(original_environ);
-        return (NULL);
-    }
-    return (new_environ);
+	original_environ = copy_environ();
+	new_environ = add_variable_to_environ(original_environ, new_variable);
+	if (!new_environ)
+	{
+		ft_free_matrix(original_environ);
+		return (NULL);
+	}
+	return (new_environ);
 }
 /*
-int main() 
+int	main(void)
 {
+	char	**env;
+	char	**tmp;
 
-    char    **env;
-    char    **tmp;
-
-    env = export("NEW_VALUE=42");
-    tmp = env;
-    while (*tmp) 
-    {
-        printf("%s\n", *tmp);
-        tmp++;
-    }
-    free_environ(env);
-    return 0;
+	env = export("NEW_VALUE=42");
+	tmp = env;
+	while (*tmp)
+	{
+		printf("%s\n", *tmp);
+		tmp++;
+	}
+	free_environ(env);
+	return (0);
 }
 */
