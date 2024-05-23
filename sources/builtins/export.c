ff4c81bc6202a0d1d179ec5449ae64157bd745e2
Bianca Santana Souza <bsantana@c1r4p1.42sp.org.br>
2024-05-23 18:38:09 -0300

    fix: unset and export

--- sources/builtins/export.c
+++ sources/builtins/export.c
@@ -1,5 +1,7 @@
 #include "../../includes/minishell.h"
 
+// Ao mandar apenas o export sem argumento deixar na ordem e printar "declare -x"
+
 void	export(t_minishell *shell, char *new_variable)
 {
 	add_variable_to_environ(shell, new_variable);
