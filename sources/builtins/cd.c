7fea1042a02beaadf3521141be3527868ae439fc
Bianca Santana Souza <bsantana@c3r2p5.42sp.org.br>
2024-05-24 15:04:12 -0300

    add cd buitint: testar no programa

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -7,18 +7,18 @@ void	cd(char *path)
 		path = getenv("HOME");
 		if (path == NULL)
 		{
-			ft_printf("cd: Could not get the home directory.\n", 2);
+			ft_printf_fd("cd: Could not get the home directory.\n", 2);
 			return ;
 		}
 	}
 	if (chdir(path) != 0)
 	{
-		ft_printf("cd: %s: No such file or directory\n", path, 2);
+		ft_printf_fd("cd: %s: No such file or directory\n", path, 2);
 		return ;
 	}
 	if (access(path, X_OK) != 0)
 	{
-		ft_printf("Permission denied.\n", 2);
+		ft_printf_fd("Permission denied.\n", 2);
 		return ;
 	}
 }
