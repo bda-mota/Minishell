af4ff121d8542924e0bf3fedeebd141867031d73
Bianca Santana Souza <bsantana@c1r4p2.42sp.org.br>
2024-05-27 17:36:52 -0300

    modified: builtins, get_input

--- includes/builtins.h
+++ includes/builtins.h
@@ -4,8 +4,10 @@
 # include "../libft/src/libft.h"
 # include "structs.h"
 
-void	find_builtins(t_tree *tree);
-void	echo(t_token **tokens);
+int		is_builtin(char *command);
+char	**split_command(char *content);
+void	find_builtins(t_minishell *shell, t_tree *tree);
+void	echo(char *args);
 void	env(t_minishell *env);
 void	pwd(void);
 void	export(t_minishell *shell, char *new_variable);
