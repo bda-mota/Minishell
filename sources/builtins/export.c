7ee8a4c8a25b7b8826ab5bac7135ed0dce29abd7
Bianca Santana Souza <bsantana@c1r4p2.42sp.org.br>
2024-05-28 12:14:05 -0300

    order env_copy in export

--- sources/builtins/export.c
+++ sources/builtins/export.c
@@ -9,6 +9,7 @@ void	export(char **env_copy, char *new_variable)
 		add_variable_to_environ(env_copy, new_variable);
 	else
 	{
+		order_env(env_copy);
 		while (env_copy[i])
 		{
 			printf("declare -x %s\n", env_copy[i]);
