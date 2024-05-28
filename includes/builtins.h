52f4cc2a24fad2cb8dd56994e0ddbeb08a7490e6
Bianca Santana Souza <bsantana@c1r4p2.42sp.org.br>
2024-05-28 18:50:12 -0300

    modified echo

--- includes/builtins.h
+++ includes/builtins.h
@@ -4,23 +4,29 @@
 # include "../libft/src/libft.h"
 # include "structs.h"
 
-int		is_builtin(char *content);
+/* GET_INPUT */
+
 void	free_split_command(char **cmd_args);
-char	**split_command(char *content);
 void	execute_builtins(t_tree *tree);
+char	**split_command(char *content);
+int		is_builtin(char *content);
+
+/* BUILTINS */
+
 void	echo(char *args);
 void	env(char **env_copy);
 void	pwd(void);
 void	export(char **env_copy, char *new_variable);
-char	**count_size_environ(void);
-char	**copy_environ(void);
-char	**create_new_environ(char **my_environ, int num_vars);
-void	add_variable_to_environ(char **env_copy, char *new_var);
 void	unset(char **env_copy, char *remove_var);
 void	cd(char *path);
 
+/* EXPORT_UTILS */
+
+void	add_variable_to_environ(char **env_copy, char *new_var);
 void	swap_strings(char **str1, char **str2);
 void	order_env(char **env_copy);
-
+char	**count_size_environ(void);
+char	**copy_environ(void);
+char	**create_new_environ(char **my_environ, int num_vars);
 
 #endif
--- sources/builtins/echo.c
+++ sources/builtins/echo.c
@@ -4,9 +4,13 @@ void	echo(char *args)
 {
 	int	i;
 	int	flag;
+	int	simple_quote;
+	int	double_quote;
 
 	i = 0;
 	flag = 0;
+	simple_quote = 0;
+	double_quote = 0;
 	while (args[i] == ' ')
 		i++;
 	if (ft_strncmp(args + i, "-n", 2) == 0
@@ -19,9 +23,53 @@ void	echo(char *args)
 	}
 	while (args[i])
 	{
+		if (args[i] == '"')
+		{
+			if (!simple_quote)
+			{
+				double_quote = 1;
+				i++;
+			}
+		}
+		if (args[i] == '\'')
+		{
+			if (!double_quote)
+			{
+				simple_quote = 1;
+				i++;
+			}
+		}
 		ft_printf_fd("%c", args[i]);
 		i++;
 	}
 	if (!flag)
 		ft_printf_fd("\n");
 }
+
+// void	echo(char *args)
+// {
+// 	int	i;
+// 	int	flag;
+
+// 	i = 0;
+// 	flag = 0;
+// 	while (args[i] == ' ')
+// 		i++;
+// 	if (ft_strncmp(args + i, "-n", 2) == 0
+// 		&& (args[i + 2] == ' ' || args[i + 2] == '\0'))
+// 	{
+// 		flag = 1;
+// 		i += 2;
+// 		while (args[i] == ' ')
+// 			i++;
+// 	}
+// 	while (args[i])
+// 	{
+// 		if (args[i] == ' ' && args[i + 1] == ' ')
+// 			i++;
+// 		ft_printf_fd("%c", args[i]);
+// 		i++;
+// 	}
+// 	if (!flag)
+// 		ft_printf_fd("\n");
+// }
