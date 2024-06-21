8d67155e4fb9f226f1c491769284141edcec4295
bsantana <biancasantana81@hotmail.com>
2024-06-21 00:24:14 -0300

    fix variable expansion with invalid name

--- sources/token/variable_expansion2.c
+++ sources/token/variable_expansion2.c
@@ -11,16 +11,16 @@ char *find_variable(char *content, int *i, char **env_copy, char *data_var)
 
     var_name = extract_var_name(content, i);
     if (var_name == NULL)
-        return data_var;
+        return (data_var);
     if (valid_name(var_name) == 0)
         data_var = handle_valid_variable(data_var, env_copy, var_name);
     else
         data_var = concatenate_invalid_variable(data_var, var_name);
     free(var_name);
-    return data_var;
+    return (data_var);
 }
 
-static char *extract_var_name(char *content, int *i) 
+static char *extract_var_name(char *content, int *i)
 {
     int start;
     int end;
@@ -32,9 +32,9 @@ static char *extract_var_name(char *content, int *i)
         end++;
     var_name = ft_strndup(content + start, end - start);
     if (var_name == NULL)
-        return NULL;
+        return (NULL);
     *i = end - 1;
-    return var_name;
+    return (var_name);
 }
 
 static int	valid_name(char *var_name)
@@ -74,7 +74,7 @@ static char	*handle_valid_variable(char *data_var, char **env_copy, char *var_na
 	return (data_var);
 }
 
-static char *concatenate_invalid_variable(char *data_var, char *var_name) 
+static char *concatenate_invalid_variable(char *data_var, char *var_name)
 {
     char *tmp;
     char *new_data_var;
